f922bad8afe2be5b84131518de85d0d327e14c6d
B-Rasmussen <brandonrasmussen91@gmail.com>
2025-04-25 14:04:54 -0700

    added style page for work history, added ternary operator to add appropriate styling to even/odd job card

--- src/components/WorkHistory.tsx
+++ src/components/WorkHistory.tsx
@@ -1,10 +1,11 @@
 import workHistory from "../data/workHistory";
+import "../style/workHistoryStyle.css";
 
 function WorkHistory() {
     return (
         <div>
             {workHistory.map((job) => (
-                <div key={job.id} className="job-card">
+                <div key={job.id} className={job.id % 2 ? "job-card-even" : "job-card-odd"}>
                     <h2>{job.companyName}</h2>
                     <h3>{job.positionTitle}</h3>
                     <p>
