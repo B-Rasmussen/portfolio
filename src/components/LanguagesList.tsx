9a88d9c580e4a5c3f74a84d8a5fdfe3291fbc44e
B-Rasmussen <brandonrasmussen91@gmail.com>
2025-05-01 23:39:23 -0700

    added styling and data for languages/libraries/frameworks used

--- src/components/LanguagesList.tsx
+++ src/components/LanguagesList.tsx
@@ -1,11 +1,11 @@
 import languagesList from "../data/languageListData";
+import "../style/languagesListStyle.css";
 
 function LanguagesList() {
     return (
-        <div>
+        <div className="languages-list-container">
             {languagesList.map((language) => (
-                <div key={language.id} className="language-card">
-                    <h2>{language.languageName}</h2>
+                <div key={language.id} className={language.id % 2 ? "language-card-even" : "language-card-odd"}>
                     {language.image && (
                         <img
                             src={language.image}
@@ -13,6 +13,7 @@ function LanguagesList() {
                             style={{ height: "50px" }}
                         />
                     )}
+                    <div className="language-name">{language.languageName}</div>
                 </div>
             ))}
         </div>
--- src/data/languageListData.tsx
+++ src/data/languageListData.tsx
@@ -12,6 +12,60 @@ const languagesList: LanguagesList[] = [
         image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
         imageAlt: "React",
     },
+    {
+        id: 2,
+        languageName: "JavaScript",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+        imageAlt: "JavaScript",
+    },
+    {
+        id: 3,
+        languageName: "TypeScript",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+        imageAlt: "TypeScript",
+    },
+    {
+        id: 4,
+        languageName: "HTML",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+        imageAlt: "HTML",
+    },
+    {
+        id: 5,
+        languageName: "CSS",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+        imageAlt: "CSS",
+    },
+    {
+        id: 6,
+        languageName: "Node.js",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+        imageAlt: "Node.js",
+    },
+    {
+        id: 7,
+        languageName: "Python",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+        imageAlt: "Python",
+    },
+    {
+        id: 8,
+        languageName: "Selenium",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg",
+        imageAlt: "Selenium",
+    },
+    {
+        id: 9,
+        languageName: "Appium",
+        image: "https://img.jsdelivr.com/github.com/appium.png",
+        imageAlt: "Appium",
+    },
+    {
+        id: 10,
+        languageName: "Playwright",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/playwright/playwright-original.svg",
+        imageAlt: "Playwright",
+    },
 ];
 
 export default languagesList;
