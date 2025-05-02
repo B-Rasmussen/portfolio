a8ed6afd7da137ed81d8c446fe6db140a6903228
B-Rasmussen <brandonrasmussen91@gmail.com>
2025-05-02 00:13:18 -0700

    new styling for work history component, rounded corners for languages

--- src/components/WorkHistory.tsx
+++ src/components/WorkHistory.tsx
@@ -9,15 +9,28 @@ function WorkHistory() {
                     key={job.id}
                     className={job.id % 2 ? "job-card-even" : "job-card-odd"}
                 >
-                    <h2 className="company-name">{job.companyName}</h2>
+                    {/* <h2 className="company-name">{job.companyName}</h2> */}
+                    <h2
+                        className={
+                            job.id % 2
+                                ? "company-name-even"
+                                : "company-name-odd"
+                        }
+                    >
+                        {job.companyName}
+                    </h2>
                     <h3 className="position">{job.positionTitle}</h3>
                     <div className="job-details">
                         <div id="job-location">{job.location}</div>
-                        <div id="job-date">{job.startDate} - {job.endDate}</div>
+                        <div id="job-date">
+                            {job.startDate} - {job.endDate}
+                        </div>
                     </div>
                     <ul className="job-list-container">
                         {job.bulletPoints?.map((item, index) => (
-                            <li key={index} className="job-bullet-points">{item}</li>
+                            <li key={index} className="job-bullet-points">
+                                {item}
+                            </li>
                         ))}
                     </ul>
                 </div>
