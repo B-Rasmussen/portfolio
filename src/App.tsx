3474e0cb2fc928f78b060a4a682f38c1ea93969c
B-Rasmussen <brandonrasmussen91@gmail.com>
2025-07-26 01:19:36 -0700

    added a check to render the correct page depending on device. Desktop and laptops will render the interactive resume first while mobile will render the static version

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { SetStateAction, useState } from "react";
+import { SetStateAction, useState, useEffect } from "react";
 import WorkHistoryPage from "./pages/WorkHistoryPage";
 import ProjectsPage from "./pages/ProjectsPage";
 import InteractiveResumePage from "./pages/InteractiveResumePage";
@@ -8,8 +8,30 @@ import SocialMediaBar from "./components/SocialMediaBar";
 // import LanguagesList from "./components/LanguagesList";
 import Button from "./components/Button";
 
+function isMobileDevice() {
+    const mobileKeywords = [
+        "Android",
+        "webOS",
+        "iPhone",
+        "iPad",
+        "iPod",
+        "BlackBerry",
+        "Windows Phone",
+        "IEMobile",
+        "Opera Mini",
+    ];
+
+    for (const keyword of mobileKeywords) {
+        if (navigator.userAgent.includes(keyword)) {
+            console.log(`Detected mobile device: ${keyword}`);
+            return true;
+        }
+    }
+    return false;
+}
+
 function App() {
-    const [currentScreen, setCurrentScreen] = useState("InteractiveResume");
+    const [currentScreen, setCurrentScreen] = useState("");
     // const [isDarkMode, setIsDarkMode] = useState(false);
 
     const navigateTo = (screenName: SetStateAction<string>) => {
@@ -19,12 +41,7 @@ function App() {
     const renderPage = () => {
         switch (currentScreen) {
             case "WorkHistory":
-                return (
-                    <div>
-                        {/* <LanguagesList /> */}
-                        <WorkHistoryPage />
-                    </div>
-                );
+                return <WorkHistoryPage />;
             case "Projects":
                 return <ProjectsPage />;
             case "InteractiveResume":
@@ -33,14 +50,32 @@ function App() {
                 return <div>Oops something went wrong</div>;
         }
     };
+
+    useEffect(() => {
+        if (isMobileDevice()) {
+            setCurrentScreen("WorkHistory");
+        } else {
+            setCurrentScreen("InteractiveResume");
+        }
+    }, []);
+
     return (
         <>
-            {currentScreen == "InteractiveResume" ? (
-                <div style={{ color: "White", backgroundColor: "green", padding: "5px", borderRadius: "50px", position: "absolute", bottom: "0.3%", left: "10%", zIndex: 1000 }}>
+            {currentScreen === "InteractiveResume"  ? (
+                <div
+                    style={{
+                        color: "White",
+                        backgroundColor: "green",
+                        padding: "5px",
+                        borderRadius: "50px",
+                        position: "absolute",
+                        bottom: "0.3%",
+                        left: "10%",
+                        zIndex: 1000,
+                    }}
+                >
                     <Button
-                        buttonName={
-                            "In a rush? Static version here!"
-                        }
+                        buttonName={"In a rush? Static version here!"}
                         onButtonPressed={() => navigateTo("WorkHistory")}
                     />
                 </div>
