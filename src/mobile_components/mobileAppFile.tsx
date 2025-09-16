f148f1fe1956e4ab754cd1a01289f8e66250b16b
B-Rasmussen <brandonrasmussen91@gmail.com>
2025-09-16 14:09:03 -0700

    small change

--- src/mobile_components/mobileAppFile.tsx
+++ src/mobile_components/mobileAppFile.tsx
@@ -8,50 +8,50 @@ export function MobileAppFile({ data, dataName }: mobileAppFileProps) {
         <div>
             {dataName === "Work History"
                 ? data.map((item: any, index: number) => (
-                      <div
-                          key={item.id}
-                          // onClick={() => {
-                          //     handleOpen(index);
-                          // }}
-                          className=""
-                      >
-                          <img
-                              src={item.companyLogo ? item.companyLogo : null}
-                              alt={item.companyLogoAlt}
-                          />
-                          <text>{item.companyName}</text>
-                      </div>
-                  ))
+                    <div
+                        key={item.id}
+                            // onClick={() => {
+                            //     handleOpen(index);
+                            // }}
+                        className=""
+                    >
+                        <img
+                            src={item.companyLogo ? item.companyLogo : null}
+                            alt={item.companyLogoAlt}
+                        />
+                        <text>{item.companyName}</text>
+                    </div>
+                ))
                 : null}
             {dataName === "Personal Projects"
                 ? data.map((item: any, index: number) => (
-                      <div
-                          key={item.id}
-                          // onClick={() => {
-                          //     handleOpen(index);
-                          // }}
-                          className=""
-                      >
-                          {/* <img
-                              src={item.companyLogo}
-                              alt={item.companyLogoAlt}
-                          /> */}
-                          <text>{item.projectName}</text>
-                      </div>
-                  ))
-                : null}
+                    <div
+                        key={item.id}
+                            // onClick={() => {
+                            //     handleOpen(index);
+                            // }}
+                        className=""
+                    >
+                        {/* <img
+                            src={item.companyLogo}
+                            alt={item.companyLogoAlt}
+                        /> */}
+                        <text>{item.projectName}</text>
+                    </div>
+                ))
+            : null}
             {dataName === "Linkedin Recommendations"
                 ? data.map((item: any, index: number) => (
-                      <div
-                          key={item.id}
-                          // onClick={() => {
-                          //     handleOpen(index);
-                          // }}
-                          className=""
-                      >
-                          <text>{item.person}</text>
-                      </div>
-                  ))
+                    <div
+                        key={item.id}
+                            // onClick={() => {
+                            //     handleOpen(index);
+                            // }}
+                        className=""
+                    >
+                        <text>{item.person}</text>
+                    </div>
+                ))
                 : null}
         </div>
     );
